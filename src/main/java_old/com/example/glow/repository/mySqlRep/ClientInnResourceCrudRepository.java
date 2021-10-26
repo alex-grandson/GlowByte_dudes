71023f2a73c3baedb87a2d9a4abc949bef1e7740
Pivnoy <evgenij_makarev20021@mail.ru>
2021-10-26 09:40:32 +0300

    STABLE

--- src/main/java_old/com/example/glow/repository/mySqlRep/ClientInnResourceCrudRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.glow.repository.mySqlRep;
-
-import com.example.glow.entity.mySql.ClientInnResource;
-import org.springframework.data.repository.CrudRepository;
-
-public interface ClientInnResourceCrudRepository extends CrudRepository<ClientInnResource, Long> {
-
-}
